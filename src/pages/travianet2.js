0d1954eb13bfa44355475f4c083c7158d2dcfceb
24mert24 <mertcanpolat24@outlook.com>
2024-12-06 16:15:34 +0100

    Create travianet2.js

---
+++ src/pages/travianet2.js
@@ -0,0 +1,17 @@
+export default function TravianetPage() {
+    return (
+        <div>
+            <h1>Reiseangebote</h1>
+            <p>
+                Hier finden Sie großartige Reiseangebote:
+            </p>
+            <a
+                href="https://www.travialinks.de/link/A-29963-0/A/trafficsTBE"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Zu Travianet
+            </a>
+        </div>
+    );
+}
